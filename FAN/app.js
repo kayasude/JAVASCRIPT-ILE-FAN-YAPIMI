7bb4f3395872578ed3499973d7912e5920e4b024
SUDE KAYA <117521906+kayasude@users.noreply.github.com>
2024-02-01 23:03:42 +0300

    Add files via upload

---
+++ FAN/app.js
@@ -0,0 +1,19 @@
+let a = document.getElementById("img");
+function on(){
+    a.style.animationDuration=3+"s";
+}
+function off(){
+    a.style.animationDuration=0+"s";
+}
+function kademe1(){
+    a.style.animationDuration=1+"s";
+}
+function kademe2(){
+    a.style.animationDuration=0.5+"s";
+}
+function kademe3(){
+    a.style.animationDuration=0.1+"s";
+}
+
+
+
